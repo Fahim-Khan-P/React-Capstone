290f4559ef46f90852fc259e6cd416508de55ded
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-02-15 23:12:17 +0430

    Add countryList component

---
+++ src/components/CountryList.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CountryCard from './Country';
+
+const CountryList = ({ data }) => (
+  <div className="CountryListMainDiv">
+    <div className="CountryListTitle">List of Urope Countries</div>
+    <div className="CountryList">
+      {data.map((country) => (
+        <Link
+          key={country.ccn3}
+          to={`details:${country.name.common}`}
+          state={{ capital: country.capital[0] }}
+        >
+          <CountryCard
+            name={country.name.common}
+            population={country.population}
+            flag={country.flags.png}
+          />
+        </Link>
+
+      ))}
+    </div>
+  </div>
+);
+
+export default CountryList;
